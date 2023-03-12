e7c56c50b465cc8f350f84c4b8daf2b2656e26c4
Abdullah-Hanouna <banckclick@gmail.com>
2023-03-12 18:17:41 +0200

    make category and veiws

--- job/models.py
+++ job/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
+
 # Create your models here.
+JOB_TYPE = (
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+    
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=13 , choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    catogry = models.ForeignKey('Category',on_delete=models.CASCADE)
+
+
+
+    def __str__(self) -> str:
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    
+
+    def __str__(self) -> str:
+        return self.name
